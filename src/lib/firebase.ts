0b86ba1da5ba9b43540fcc6c72d9392da89e824a
kanhaarajput <lalitthakur894@gmail.com>
2025-08-23 17:45:06 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBcxWxYuShrazEITqGyXsw1DCChlfQpT-Y",
-  authDomain: "my-portfolio-c205c.firebaseapp.com",
-  projectId: "my-portfolio-c205c",
-  storageBucket: "my-portfolio-c205c.firebasestorage.app",
-  messagingSenderId: "576078413639",
-  appId: "1:576078413639:web:9987e0858f60d9d4cf9662",
-  measurementId: "G-87L4ZJ4HKT"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
